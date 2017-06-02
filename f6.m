215077572e53aa4b31cceb8ee27104e057370b48
fernanIng <fernanherrero@gmail.com>
2017-06-02 17:36:28 +0200

    updatef69
    2 opciones docena par impar

--- f6.m
+++ f6.m
@@ -1,18 +1,49 @@
-dinero=[];
-dinero(1)=100;
-iter=2;
-while(dinero>0)
-    %plot(dinero)
+function y = f9 (x,y)
+    % x = dinero
+    switch y
+        case 0
+            disp('Apostando a las docenas')
+            dinero=[];
+            dinero(1)=x;
+            iter=2;
+            %docenas
+            while(dinero>0)
+                %plot(dinero)
+
+                aleatorio=randi(37);
+                if aleatorio <= 12
+                    dinero(iter)=dinero(iter-1)+2;
+                else
+                    dinero(iter)=dinero(iter-1)-1;
+                end
+                if(dinero(iter)>=1050)
+                    break
+                end
+                iter = iter+1;
+
+            end
+            iter
+            plot(dinero)
+        case 1
+            disp('Apostando a Rojo/Negro Par/Impar')
+            dinero=[];
+            dinero(1)=x;
+            iter=2;
+            while(dinero>0)
+                %plot(dinero)
     
-    dinero(iter)=dinero(iter-1)-1;
-    aleatorio=randi(37);
-    if aleatorio <= 18
-        dinero(iter)=dinero(iter)+2;
-    end
-    iter = iter+1;
+                dinero(iter)=dinero(iter-1)-1;
+                aleatorio=randi(37);
+                if aleatorio <= 18
+                    dinero(iter)=dinero(iter)+2;
+                end
+                iter = iter+1;
    
+            end
+            iter
+            plot(dinero)
+            
+    end
 end
-iter
-plot(dinero)
 
 

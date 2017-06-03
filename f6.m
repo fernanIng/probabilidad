94dcb98258f99099982ea5ba0a3e2c461b43b232
fernanIng <fernanherrero@gmail.com>
2017-06-03 20:49:29 +0200

    Add files via upload

--- f6.m
+++ f6.m
@@ -1,11 +1,11 @@
 function y = f9 (x,y)
     % x = dinero
+    dinero=[];
+    dinero(1)=x;
+    iter=2;
     switch y
         case 0
             disp('Apostando a las docenas')
-            dinero=[];
-            dinero(1)=x;
-            iter=2;
             %docenas
             while(dinero>0)
                 %plot(dinero)
@@ -26,9 +26,6 @@ function y = f9 (x,y)
             plot(dinero)
         case 1
             disp('Apostando a Rojo/Negro Par/Impar')
-            dinero=[];
-            dinero(1)=x;
-            iter=2;
             while(dinero>0)
                 %plot(dinero)
     
@@ -42,8 +39,85 @@ function y = f9 (x,y)
             end
             iter
             plot(dinero)
+        case 2
+            duplicado = 1;
+            disp('Double up')
             
+            while(dinero>0)
+                if(dinero(iter-1)<duplicado)
+                    break
+                end
+                dinero(iter)=dinero(iter-1) - duplicado;
+                aleatorio=randi(37);
+                if aleatorio <= 18
+                    dinero(iter)=dinero(iter) + (duplicado*2);
+                    duplicado=1;
+                else
+                    duplicado=duplicado*2;
+                end
+                
+                if(dinero(iter)>=120)
+                    %objetivo ganr 20$
+                    break
+                end
+                iter = iter+1;
+
+            end
+            iter
+            plot(dinero)
+        case 3
+            duplicado = 1;
+            disp('The Grand Martingale Strategy.')
+            
+            while(dinero>0)
+                if(dinero(iter-1)<duplicado)
+                    break
+                end
+                dinero(iter)=dinero(iter-1) - duplicado;
+                aleatorio=randi(37);
+                if aleatorio <= 18
+                    dinero(iter)=dinero(iter) + (duplicado*2);
+                    duplicado=1;
+                else
+                    duplicado=(duplicado*2)+1;
+                end
+                
+                if(dinero(iter)>=120)
+                    break
+                end
+                iter = iter+1;
+
+            end
+            iter
+            plot(dinero)
+            
+       case 4
+            
+            disp('James Bond Roulette Strategy.')
+            %requiere un minimo de 200$ por apuesta
+            while(dinero>=200)
+                aleatorio=randi(37);
+                if aleatorio >= 19 && aleatorio <= 36
+                    disp('de 19 a 36 -----apostamos 140$      multiplica por 2')
+                    dinero(iter)=dinero(iter-1) + 80 ;
+                elseif aleatorio >= 13 && aleatorio <= 18
+                    disp('de 13 a 18 ------apostamos 50$       multiplica por 6')
+                    dinero(iter)=dinero(iter-1) + 100 ;
+                elseif aleatorio == 37 
+                    disp('si es 0 ----- apostamos 10$      multiplica por 36')
+                    dinero(iter)=dinero(iter-1) + 160 ;
+                else
+                    dinero(iter)=dinero(iter-1) - 200 ;
+                end
+                
+                if(dinero(iter)>=600)
+                    break
+                end
+                iter = iter+1; 
+                
+            end
+            
+            iter
+            plot(dinero)
     end
 end
-
-
